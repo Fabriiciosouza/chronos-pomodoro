9e6cec68e0dd395962174d357e367cd3b489b955
Fabricio Souza <fabriciodesouza956@gmail.com>
2025-05-08 18:35:10 -0300

    importando e usando arquivos de CSS globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-export function App(){
-   console.log("oi");
+import './styles/theme.css'
+import './styles/global.css'
 
+export function App() {
+  console.log('oi')
 
-   return (
-      //<div> esse <> </> é um fragmento, ou seja, não renderiza nada no DOM só serve para agrupar elementos como div
-      <>
-         <h1>Hello World</h1>
-         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Minus voluptates impedit ad iure consectetur unde ea
-             veritatis quos. Reiciendis amet nemo deserunt illum qui 
-             impedit voluptatum omnis quod saepe quasi?
-         </p>
-      </>
-      //</div>
-   );
+  return (
+    //<div> esse <> </> é um fragmento, ou seja, não renderiza nada no DOM só serve para agrupar elementos como div
+    <>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus
+        voluptates impedit ad iure consectetur unde ea veritatis quos.
+        Reiciendis amet nemo deserunt illum qui impedit voluptatum omnis quod
+        saepe quasi?
+      </p>
+    </>
+    //</div>
+  )
 }
