a0d47c119bff21059c320de6aa5410e5c04f4ace
Fabricio Souza <fabriciodesouza956@gmail.com>
2025-05-08 18:22:34 -0300

    Primeira exportação react

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export function App(){
+   console.log("oi");
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   return (
+      //<div> esse <> </> é um fragmento, ou seja, não renderiza nada no DOM só serve para agrupar elementos como div
+      <>
+         <h1>Hello World</h1>
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Minus voluptates impedit ad iure consectetur unde ea
+             veritatis quos. Reiciendis amet nemo deserunt illum qui 
+             impedit voluptatum omnis quod saepe quasi?
+         </p>
+      </>
+      //</div>
+   );
 }
-
-export default App
