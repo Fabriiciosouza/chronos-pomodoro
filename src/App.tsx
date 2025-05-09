785589bb60b23df8e2b6b5c94e34802847c6ec3e
Fabricio Souza <fabriciodesouza956@gmail.com>
2025-05-09 14:02:37 -0300

    Criação do input e css do input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './styles/theme.css'
-import './styles/global.css'
+import './styles/theme.css';
+import './styles/global.css';
 
-import { Container } from './components/Container'
-import { Logo } from './components/Logo'
-import { Menu } from './components/Menu'
-import { CountDown } from './components/CountDown'
-
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -21,6 +21,27 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
+
+      <Container>
+        <form className='form'>
+          <div className='formRow'>
+            <DefaultInput labeltext='task' id='meuImput' type='text' placeholder='Digite algo'/>
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
-  )
+  );
 }
---
+++ src/components/DefaultInput/index.tsx
@@ -0,0 +1,20 @@
+import styles from './styles.module.css';
+
+type DefaultInputProps = {
+  id: string;
+  labeltext?: string;
+} & React.ComponentProps<'input'>;
+
+export function DefaultInput({
+  id,
+  type,
+  labeltext,
+  ...rest
+}: DefaultInputProps) {
+  return (
+    <>
+      {labeltext && <label htmlFor={id}>{labeltext}</label>}
+      <input className={styles.input} id={id} type={type} {...rest} />
+    </>
+  );
+}
